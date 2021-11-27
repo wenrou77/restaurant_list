22c81799add5540e4592d6d2fe3288b0375aaf34
wenrou77 <wendyog@gmail.com>
2021-11-27 19:22:36 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
+const app = express()
+const port = 3000
+
+app.use(express.static('public'))
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.get('/', (req, res) => {
+  res.render('index', { restaurant: restaurantList.results })
+})
+
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+  res.render('show', { restaurant: restaurant })
+})
+
+app.get('/search', (req, res) => {
+  ///Version 1. 只能搜尋餐廳名字或類別
+  //const keyword = req.query.keyword
+  // const restaurants = restaurantList.results.filter(restaurant => {
+  //   return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.toLowerCase().includes(keyword.toLowerCase())
+  // })
+
+  ///Version 2. 能夠同時搜尋餐廳名字和類別（較寬鬆的篩選，只要其中一個條件符合就渲染出來）
+  //get each keyword
+  const keywords = req.query.keyword.toLowerCase().trim().split(' ')
+  //open new array
+  let restaurantChosen = []
+  //double loop to find the chosen restaurant
+  const restaurants = restaurantList.results.filter(restaurant => {
+    keywords.forEach(item => {
+      if (restaurant.name.toLowerCase().includes(item) || restaurant.category.toLowerCase().includes(item)) {
+        restaurantChosen.push(restaurant)
+      }
+    })
+  })
+  //去除陣列中的重複項
+  restaurantChosen = [...new Set(restaurantChosen)]
+  res.render('index', { restaurant: restaurantChosen, keyword: keywords })
+})
+
+app.listen(port, () => {
+  console.log(`Express is listening on localhost:${port}`)
+})
