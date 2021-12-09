ff331a626699fa9547f10febfc347066f6179606
wenrou77 <wendyog@gmail.com>
2021-12-09 20:40:27 +0800

    feat: add todo seeder

--- models/restaurant.js
+++ models/restaurant.js
@@ -1,9 +1,40 @@
 const mongoose = require('mongoose')
+
+
 const Schema = mongoose.Schema
-const todoSchema = new Schema({
+const restaurantSchema = new Schema({
+  id: {
+    type: String
+  },
   name: {
     type: String, // 資料型別是字串
     required: true // 這是個必填欄位
-  }
+  },
+  name_en: {
+    type: String
+  },
+  category: {
+    type: String
+  },
+  image: {
+    type: String
+  },
+  location: {
+    type: String
+  },
+  phone: {
+    type: String
+  },
+  google_map: {
+    type: String
+  },
+  rating: {
+    type: Number
+  },
+  description: {
+    type: String
+  },
 })
-module.exports = mongoose.model('Restaurant', todoSchema)
+
+
+module.exports = mongoose.model('Restaurant', restaurantSchema)
---
+++ models/seeds/restaurantSeeder.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const Restaurant = require('../restaurant')
+const restaurantList = require('../../restaurant.json')
+mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
+restaurantNum = restaurantList.results.length
+
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  for (let i = 0; i < restaurantNum - 1; i++) {
+    Restaurant.create(restaurantList.results[i])
+  }
+  console.log('mongodb connected!')
+})
+
+//console.log(restaurantList.results[0].name)
